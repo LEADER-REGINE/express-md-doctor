ccf6c12fd40639e22e6a3393433b28cf0c119a13
Marc Angelo Chiapco <marcchiapco34@gmail.com>
2022-02-03 09:55:59 +0800

    Update Profile.js

--- src/pages/App/Profile.js
+++ src/pages/App/Profile.js
@@ -20,6 +20,20 @@ const user = auth.currentUser;
 
 function Profile() {
 
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.User
+            var uid = user.uid;
+            localStorage.setItem("uid", uid);
+            // ...
+        } else {
+            // User is signed out
+            // ...
+            history.push("/login");
+        }
+    });
+
     const id = localStorage.getItem("uid");
     const history = useHistory();
     const db = firebase.firestore();
